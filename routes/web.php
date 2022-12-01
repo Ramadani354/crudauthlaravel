7bceb80dd9f7090aa2c4eed990484cf0602a2e56
yoviadhi123@gmail.com <yoviadhi123@gmail.com>
2022-12-01 15:19:14 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\MahasiswaController;
 
@@ -14,9 +15,10 @@ use App\Http\controllers\MahasiswaController;
 |
 */
 
-//Route::get('/', function () {
-//   return view('welcome');
-//});
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/read',[MahasiswaController::class, 'index'])->name('read');
 Route::get('/create',[MahasiswaController::class, 'create'])->name('create');
@@ -24,3 +26,16 @@ Route::post('/insert',[MahasiswaController::class, 'insert'])->name('insert');
 Route::get('/edit/{nim}',[MahasiswaController::class, 'edit'])->name('edit');
 Route::post('/update/{nim}',[MahasiswaController::class, 'update'])->name('update');
 Route::get('/delete/{nim}',[MahasiswaController::class, 'delete'])->name('delete');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
